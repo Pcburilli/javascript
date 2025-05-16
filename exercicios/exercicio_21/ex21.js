cd3a34028d58e466c0685a9ab88a0146e79ec6a8
Pedro Costa Burilli <105596677+Pcburilli@users.noreply.github.com>
2025-05-16 11:18:50 -0300

    melhorando ex21

--- exercicios/exercicio_21/ex21.js
+++ exercicios/exercicio_21/ex21.js
@@ -1,36 +1,48 @@
 let adc = document.getElementById('adc')
 let fin = document.getElementById('fin')
 let tab = document.getElementById('res')
+let num_id = document.getElementById('numero')
 let valores = []
 
 adc.addEventListener('click',adicionar)
+num_id.addEventListener('keydown', function(event) {
+    if (event.key === 'Enter') {
+        adicionar()
+    }
+})
 fin.addEventListener('click',finalizar)
 
 function adicionar(){
-    let num = Number(document.getElementById('numero').value)
-
+    let num = Number(num_id.value)
     if (num < 1 || num > 100 || isNaN(num)){
         alert('Número digitado está incorreto ou não foi digitado.')
     } else if (valores.indexOf(num) != -1) {
         alert('O número já foi digitado.')
     } else {
-        tab.innerHTML += `<option>Valor ${num} adicionado.</option>`
         valores.push(num)
+        tab.innerHTML += `<option>Valor ${num} adicionado.</option>`
         let res = document.getElementById('res2').innerHTML = ''
     }
+    num_id.value = ''
+    num_id.focus()
 }
 
 function finalizar(){
-    valores.sort()
-    let res = document.getElementById('res2')
-    let total = 0
-    for (let x = 0; x < valores.length; x++){
-        total = total + valores[x]
+    if (valores.length == 0){
+        alert('Não é possível finalizar.')
+    } else{
+        valores.sort((a,b) => a - b)
+        let res = document.getElementById('res2')
+        let total = 0
+        for (let x = 0; x < valores.length; x++){
+            total = total + valores[x]
+        }
+        let media = total / valores.length
+        res.innerHTML = `
+            <p class="p_res">Ao todo, temos ${valores.length} números cadastrados.</p>
+            <p class="p_res">O maior valor informado foi ${valores[valores.length - 1]}.</p>
+            <p class="p_res">O menor valor informado foi ${valores[0]}.</p>
+            <p class="p_res">Somando todos os valores, temos ${total}.</p>
+            <p class="p_res">A média dos valores digitados é ${media.toFixed(2)}.</p>`
     }
-    let media = total / valores.length
-    res.innerHTML = `Ao todo, temos ${valores.length} números cadastrados`
-    res.innerHTML += `<br><br>O maior valor informado foi ${valores[valores.length - 1]}.`
-    res.innerHTML += `<br><br>O menor valor informado foi ${valores[0]}.`
-    res.innerHTML += `<br><br>Somando todos os valores, temos ${total}.`
-    res.innerHTML += `<br><br>A média dos valores digitados é ${media.toFixed(2)}.`
 }
